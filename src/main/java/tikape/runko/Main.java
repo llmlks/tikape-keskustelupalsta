4df5c9af93c6764a6092d3944a4d622032260b53
Melkas Laila <lmelkas@da5-cs-b221-13.helsinki.fi>
2016-10-20 16:02:49 +0300

    main: Herokua varten portti-check

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -18,6 +18,11 @@ import tikape.runko.domain.Viesti;
 public class Main {
     
     public static void main(String[] args) throws Exception {
+        
+        // asetetaan portti jos heroku antaa PORT-ympäristömuuttujan
+        if (System.getenv("PORT") != null) {
+            port(Integer.valueOf(System.getenv("PORT")));
+        }
 
         Database database = new Database("jdbc:sqlite:testi.db");
 
